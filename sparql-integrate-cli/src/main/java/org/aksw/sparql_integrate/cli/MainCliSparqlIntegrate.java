b056bec70dfee6240185b6021dcb346e1c46cefe
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-04-23 10:29:47 +0200

    Added a 'jq' format

--- sparql-integrate-cli/src/main/java/org/aksw/sparql_integrate/cli/MainCliSparqlIntegrate.java
+++ sparql-integrate-cli/src/main/java/org/aksw/sparql_integrate/cli/MainCliSparqlIntegrate.java
@@ -7,33 +7,47 @@ import java.io.InputStream;
 import java.io.SequenceInputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.aksw.jena_sparql_api.core.SparqlService;
 import org.aksw.jena_sparql_api.server.utils.FactoryBeanSparqlServer;
 import org.aksw.jena_sparql_api.sparql.ext.fs.JenaExtensionFs;
 import org.aksw.jena_sparql_api.sparql.ext.http.JenaExtensionHttp;
 import org.aksw.jena_sparql_api.sparql.ext.util.JenaExtensionUtil;
+import org.aksw.jena_sparql_api.stmt.SPARQLResultSink;
+import org.aksw.jena_sparql_api.stmt.SPARQLResultSinkQuads;
+import org.aksw.jena_sparql_api.stmt.SPARQLResultVisitor;
 import org.aksw.jena_sparql_api.stmt.SparqlStmt;
 import org.aksw.jena_sparql_api.stmt.SparqlStmtParserImpl;
 import org.aksw.jena_sparql_api.stmt.SparqlStmtUtils;
 import org.aksw.jena_sparql_api.update.FluentSparqlService;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.jena.atlas.lib.Sink;
+import org.apache.jena.graph.Node;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.Syntax;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.riot.Lang;
@@ -62,12 +76,165 @@ import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.StandardSystemProperty;
 import com.google.common.base.Stopwatch;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 @SpringBootApplication
 public class MainCliSparqlIntegrate {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainCliSparqlIntegrate.class);
 
+	public static class SPARQLResultVisitorSelectJsonOutput
+		implements SPARQLResultSink
+	{
+		protected JsonArray arr;
+		protected int maxDepth = 3;
+		protected Gson gson;
+		
+		public SPARQLResultVisitorSelectJsonOutput() {
+			this(null, null, null);
+		}
+
+		public SPARQLResultVisitorSelectJsonOutput(Gson gson) {
+			this(null, null, gson);
+		}
+		
+		public SPARQLResultVisitorSelectJsonOutput(JsonArray arr, Integer maxDepth, Gson gson) {
+			super();
+			this.arr = arr != null ? arr : new JsonArray();
+			this.maxDepth = maxDepth != null ? maxDepth : 3;
+			this.gson = gson != null ? gson : new Gson();
+		}
+
+		@Override
+		public void onResultSet(ResultSet value) {
+			JsonElement json = toJson(value, maxDepth);
+			onJson(json);
+		}
+
+		@Override
+		public void onJson(JsonElement value) {
+			//String str = gson.toJson(value);
+			arr.add(value);
+		}
+
+		@Override
+		public void onQuad(Quad value) {
+			System.err.println(value);
+		}
+
+		@Override
+		public void close() throws Exception {
+			// Return the last item of the json array
+			JsonElement tmp = arr.size() == 0
+					? new JsonObject()
+					: arr.get(arr.size() - 1);
+//			JsonElement tmp = arr.size() != 1
+//					? arr
+//					: arr.get(0);
+
+			String str = gson.toJson(tmp);
+			System.out.println(str);
+		}
+
+
+		@Override
+		public void flush() {
+		}
+	}
+	
+	
+	public static JsonElement toJson(RDFNode rdfNode, int maxDepth) {
+		JsonElement result = toJson(rdfNode, 0, maxDepth);
+		return result;
+	}
+
+	public static JsonObject toJson(Resource rdfNode, int maxDepth) {
+		JsonElement tmp = toJson(rdfNode, 0, maxDepth);
+		JsonObject result = tmp.getAsJsonObject();
+		return result;
+	}
+	
+	public static JsonArray toJson(ResultSet rs, int maxDepth) {
+		JsonArray result = new JsonArray();
+		List<String> vars = rs.getResultVars();
+		while(rs.hasNext()) {
+			JsonObject row = new JsonObject();
+			QuerySolution qs = rs.next();
+			for(String var : vars) {
+				RDFNode rdfNode = qs.get(var);
+				JsonElement jsonElement = toJson(rdfNode, maxDepth);
+				row.add(var, jsonElement);
+			}
+			result.add(row);
+		}
+		
+		return result;
+	}
+	
+	public static JsonElement toJson(RDFNode rdfNode, int depth, int maxDepth) {
+		JsonElement result;
+
+		if(depth >= maxDepth) {
+			// TODO We could add properties indicating that data was cut off here
+			result = new JsonObject();
+		} else if(rdfNode == null) { 
+			result = JsonNull.INSTANCE;
+		} else if(rdfNode.isLiteral()) {
+			Node node = rdfNode.asNode();
+			Object obj = node.getLiteralValue();
+			//boolean isNumber =//NodeMapperRdfDatatype.canMapCore(node, Number.class);
+			//if(isNumber) {
+			if(obj instanceof Number) {
+				Number value = (Number)obj; //NodeMapperRdfDatatype.toJavaCore(node, Number.class);
+//				Literal literal = rdfNode.asLiteral();
+				result = new JsonPrimitive(value);	
+			} else if(obj instanceof String) {
+				String value = (String)obj;
+				result = new JsonPrimitive(value);
+			} else {
+				throw new RuntimeException("Unsupported literal: " + rdfNode);
+			}
+		} else if(rdfNode.isResource()) {
+			JsonObject tmp = new JsonObject();
+			Resource r = rdfNode.asResource();
+			
+			if(r.isURIResource()) {
+				tmp.addProperty("@id", r.getURI());
+			} else if(r.isAnon()) {
+				tmp.addProperty("@id", r.getId().getLabelString());				
+			}
+			
+			List<Statement> stmts = r.listProperties().toList();
+			
+			Map<Property, List<RDFNode>> pos = stmts.stream()
+					.collect(Collectors.groupingBy(Statement::getPredicate,
+							Collectors.mapping(Statement::getObject, Collectors.toList())));
+
+			for(Entry<Property, List<RDFNode>> e : pos.entrySet()) {
+				JsonArray arr = new JsonArray();
+				Property p = e.getKey();
+				String k = p.getLocalName();
+				
+				for(RDFNode o : e.getValue()) {
+					JsonElement v = toJson(o, depth + 1, maxDepth);
+					arr.add(v);
+				}
+				
+				tmp.add(k, arr);
+			}
+			result = tmp;
+		} else {
+			throw new RuntimeException("Unknown node type: " + rdfNode);
+		}
+		
+		return result;
+	}
 	
 	
 	public static Model parseTurtleAgainstModel(Model model, PrefixMapping prefixMapping, InputStream in) {
@@ -95,23 +262,68 @@ public class MainCliSparqlIntegrate {
 		@Bean
 		public ApplicationRunner applicationRunner() {
 			return (args) -> {
+				Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+				if(false)
+				{
+					Model model = RDFDataMgr.loadModel("/home/raven/Projects/limbo/git/claus-playground-dataset/04-dcat-sparql/target/release.dcat.ttl");
+					//RDFDataMgr.write(System.out, model, RDFFormat.JSONLD);
+					
+					try(QueryExecution qe = QueryExecutionFactory.create("SELECT ?ds ?dist { ?ds <http://www.w3.org/ns/dcat#distribution> ?dist }", model)) {
+						ResultSet rs = qe.execSelect();
+						JsonArray jsonElement = toJson(rs, 3);
+//						while(rs.hasNext()) {
+//							JsonElement jsonElement = toJson(rs.next().get("s"), 3);
+//							String str = gson.toJson(jsonElement);
+//							
+//							System.out.println(str);
+//						}
+						String str = gson.toJson(jsonElement);
+						
+						System.out.println(str);
+
+						//String str = ResultSetFormatter..asText(rs);
+						//System.out.println(str);
+//						JsonLDWriter writer = new JsonLDWriter(RDFFormat.JSONLD_PRETTY);
+//						writer.wri
+					}
+					if(true) { return; };
+				}
 				
 				Collection<RDFFormat> available = RDFWriterRegistry.registered();
-				String optOutFormat = Optional.ofNullable(args.getOptionValues("w")).map(x -> x.iterator().next()).orElse(null);
+				String tmpOutFormat = Optional.ofNullable(args.getOptionValues("w"))
+						.orElse(Collections.emptyList()).stream()
+						.findFirst().orElse(null);
 
+				String optOutFormat = args.containsOption("jq")
+						? "jq"
+						: tmpOutFormat;
+				
 				// TODO Maybe show algebra is better for trace logging?
 				boolean showAlgebra = args.containsOption("a");
 				
+				SPARQLResultSink sink;
 				RDFFormat outFormat = null;
 				if(optOutFormat != null) {
-			        outFormat = available.stream()
-			        		.filter(f -> f.toString().equalsIgnoreCase(optOutFormat))
-			        		.findFirst()
-							.orElseThrow(() -> new RuntimeException("Unknown format: " + optOutFormat + " Available: " + available));
+					if(optOutFormat.equals("jq")) {
+						sink = new SPARQLResultVisitorSelectJsonOutput(gson);
+					} else {
+				        outFormat = available.stream()
+				        		.filter(f -> f.toString().equalsIgnoreCase(optOutFormat))
+				        		.findFirst()
+								.orElseThrow(() -> new RuntimeException("Unknown format: " + optOutFormat + " Available: " + available));
+				        
+						Sink<Quad> quadSink = SparqlStmtUtils.createSink(outFormat, System.out);
+						sink = new SPARQLResultSinkQuads(quadSink);
+					}			        
+				} else {
+					Sink<Quad> quadSink = SparqlStmtUtils.createSink(outFormat, System.out);
+					sink = new SPARQLResultSinkQuads(quadSink);
 				}
 //				System.out.println(outFormat);
 				
-				Sink<Quad> sink = SparqlStmtUtils.createSink(outFormat, System.out);
+				//SPARQLResultVisitor
+				//Sink<Quad> sink = SparqlStmtUtils.createSink(outFormat, System.out);
 				
 
 				// TODO Replace with our RDFDataMgrEx 
@@ -176,7 +388,9 @@ public class MainCliSparqlIntegrate {
 							InputStream in = SparqlStmtUtils.openInputStream(filename);
 							// FIXME Validate we are really using turtle here
 							parseTurtleAgainstModel(tmp, pm, in);
-
+							// Copy any prefixes from the parse back to our global prefix mapping
+							pm.setNsPrefixes(tmp);
+							
 //							tmp.setNsPrefixes(pm);
 //							RDFDataMgr.read(tmp, filename);
 							
@@ -187,7 +401,7 @@ public class MainCliSparqlIntegrate {
 							
 							String baseIri = cwd == null ? null : cwd.toUri().toString();
 							SparqlStmtUtils.processFile(pm, filename, baseIri)
-								.forEach(stmt -> processSparqlStmtWrapper(conn, stmt, showAlgebra, sink::send));
+								.forEach(stmt -> processSparqlStmtWrapper(conn, stmt, showAlgebra, sink));
 						}
 					}
 				}
@@ -243,30 +457,37 @@ public class MainCliSparqlIntegrate {
 		}
 	}
 
-	public static void processSparqlStmtWrapper(RDFConnection conn, SparqlStmt stmt, boolean showAlgebra, Consumer<Quad> sink) {
+	public static void processSparqlStmtWrapper(RDFConnection conn, SparqlStmt stmt, boolean showAlgebra, SPARQLResultVisitor sink) {
 		Stopwatch sw2 = Stopwatch.createStarted();
 		processSparqlStmt(conn, stmt, showAlgebra, sink);
 		logger.info("SPARQL stmt execution finished after " + sw2.stop().elapsed(TimeUnit.MILLISECONDS) + "ms");
 	}
 
-	public static void processSparqlStmt(RDFConnection conn, SparqlStmt stmt, boolean showAlgebra, Consumer<Quad> sink) {
+	public static Op toAlgebra(SparqlStmt stmt) {
+		Op result = null;
+
+		if(stmt.isQuery()) {
+			Query q = stmt.getAsQueryStmt().getQuery();
+			result = Algebra.compile(q);
+		} else if(stmt.isUpdateRequest()) {
+			UpdateRequest ur = stmt.getAsUpdateStmt().getUpdateRequest();
+			for(Update u : ur) {
+				if(u instanceof UpdateModify) {
+					Element e = ((UpdateModify)u).getWherePattern();
+					result = Algebra.compile(e);
+				}
+			}
+		}
+
+		return result;
+	}
+	
+	public static void processSparqlStmt(RDFConnection conn, SparqlStmt stmt, boolean showAlgebra, SPARQLResultVisitor sink) {
 		logger.info("Processing SPARQL Statement: " + stmt);
 		
 		if(showAlgebra) {
-			if(stmt.isQuery()) {
-				Query q = stmt.getAsQueryStmt().getQuery();
-				Op op = Algebra.compile(q);
-				logger.info("Algebra: " + op);
-			} else if(stmt.isUpdateRequest()) {
-				UpdateRequest ur = stmt.getAsUpdateStmt().getUpdateRequest();
-				for(Update u : ur) {
-					if(u instanceof UpdateModify) {
-						Element e = ((UpdateModify)u).getWherePattern();
-						Op op = Algebra.compile(e);
-						logger.info("Algebra: " + op);					
-					}
-				}
-			}
+			Op op = toAlgebra(stmt);
+			logger.info("Algebra: " + op);
 		}
 		SparqlStmtUtils.process(conn, stmt, sink);
 	}
