9976cb6892886e3f8d0d52e469369ca7a959de4c
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-02-22 05:38:21 +0100

    Updated benchmark resources

--- ngs/ngs-benchmark.sh
+++ ngs/ngs-benchmark.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#zsh time output looks better imo
+#/bin/bash
 
-time ngs map --sparql even.sparql test-data.trig > /tmp/ngs-bench-map.trig
+data="$1"
+query="$2"
+
+time ngs map --sparql "$query" "$data" > /tmp/ngs-bench-map.trig
 
 # Note: --u for union default graph mode not needed as the query is graph aware
 # Note --w=trig/pretty is too slow - see https://issues.apache.org/jira/browse/JENA-1848
 # time sparql-integrate --w=trig/pretty test-data.trig even.sparql > /tmp/ngs-bench-si.trig
-time sparql-integrate test-data.trig even.sparql > /tmp/ngs-bench-si.trig
+time sparql-integrate "$data" "$query" > /tmp/ngs-bench-si.trig
