8a9a352bf5fe9c2937912d7c27a083225803d711
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-02-27 18:04:29 +0100

    Improved trig boundary detector

--- sparql-integrate-cli/src/main/java/org/aksw/sparql_integrate/ngs/cli/MainPlaygroundScanTrig.java
+++ sparql-integrate-cli/src/main/java/org/aksw/sparql_integrate/ngs/cli/MainPlaygroundScanTrig.java
@@ -7,52 +7,56 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.aksw.jena_sparql_api.io.binseach.BoyerMooreMatcherFactory;
+import org.aksw.jena_sparql_api.io.binseach.CharSequenceFromSeekable;
 import org.aksw.jena_sparql_api.io.binseach.PageManager;
 import org.aksw.jena_sparql_api.io.binseach.PageManagerForFileChannel;
 import org.aksw.jena_sparql_api.io.binseach.PageNavigator;
-import org.aksw.jena_sparql_api.io.binseach.SeekableMatcher;
 import org.aksw.jena_sparql_api.rx.RDFDataMgrRx;
 import org.apache.jena.riot.Lang;
 
 public class MainPlaygroundScanTrig {
 
 	public static void main(String[] args) throws IOException {
+		// Pattern to find the start of named graphs in trig
+		Pattern trigStartPattern = Pattern.compile("@base|@prefix|(graph)?\\s*(<[^>]*>|_:[^-\\s])\\s*\\{");
+
+		
 		Path path = Paths.get("/home/raven/Projects/Eclipse/sparql-integrate-parent/ngs/one-week.trig");
 
 		//PageManager pageManager = new PageManagerForByteBuffer(ByteBuffer.wrap(text));
 		try(FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.READ)) {
 			PageManager pageManager = PageManagerForFileChannel.create(fileChannel);
-
 			PageNavigator nav = new PageNavigator(pageManager);
+
+			CharSequence charSequence = new CharSequenceFromSeekable(nav);
 			
-			// TODO Matcher needs support for horizon argument (maximum number of bytes to scan)
-			SeekableMatcher findOpenCurlyBrace = BoyerMooreMatcherFactory.createFwd("{".getBytes()).newMatcher();
-			SeekableMatcher findOpenAngularBracket = BoyerMooreMatcherFactory.createBwd("<".getBytes()).newMatcher();			
+			Matcher m = trigStartPattern.matcher(charSequence);
+			int matchCount = 0;
+			while(m.find() && matchCount < 5) {
+				int start = m.start();
+				nav.nextPos(start);
+
+				long absPos = nav.getPos();
+				
+				PageNavigator clonedNav = nav.clone();
+				InputStream in = Channels.newInputStream(clonedNav);
+				int maxQuadCount = 100;
+				long quadCount = RDFDataMgrRx.createFlowableQuads(() -> in, Lang.TRIG, null)
+					.limit(maxQuadCount)
+					.count()
+					.blockingGet();
 			
-			while(findOpenCurlyBrace.find(nav)) {
-				long candPos = nav.getPos();
-				
-				// TODO Matchers should probably return the relative position so
-				// that we can seek without requiring the notion of an absolute position
-				// if((relPos = matcher.find(nav)) != -1)  
-				if(findOpenAngularBracket.find(nav)) {
-				
-					InputStream in = Channels.newInputStream(nav);
-					int maxCount = 100;
-					long count = RDFDataMgrRx.createFlowableQuads(() -> in, Lang.TRIG, null)
-						.limit(maxCount)
-						.count()
-						.blockingGet();
-				
-					if(count != 0) {
-						System.out.println("At pos " + candPos + " found " + count + "/" + maxCount + " quads");
-					}
-					nav.setPos(candPos + 1);
+				if(quadCount != 0) {
+					++matchCount;
+					System.out.println("Candidate start pos " + absPos + " yeld " + quadCount + "/" + maxQuadCount + " quads");
 				}
 
 			}
+
+			System.out.println("Ended at " + nav.getPos());
 		}
 
 	}
