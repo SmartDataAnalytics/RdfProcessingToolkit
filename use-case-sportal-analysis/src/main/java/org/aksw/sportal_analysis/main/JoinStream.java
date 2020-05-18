0e78d6e0feb410b8ee1951e270cd041bc111c837
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-05-18 15:55:52 +0200

    Implemented push-based query execution for group by, extend/assign and project

--- use-case-sportal-analysis/src/main/java/org/aksw/sportal_analysis/main/JoinStream.java
+++ use-case-sportal-analysis/src/main/java/org/aksw/sportal_analysis/main/JoinStream.java
@@ -1,34 +1,53 @@
 package org.aksw.sportal_analysis.main;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.aksw.jena_sparql_api.rx.SparqlRx;
 import org.apache.jena.atlas.lib.Pair;
-import org.apache.jena.atlas.lib.Sink;
 import org.apache.jena.ext.com.google.common.collect.Multimap;
 import org.apache.jena.ext.com.google.common.collect.MultimapBuilder;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
+import org.apache.jena.query.ARQ;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.rdfconnection.SparqlQueryConnection;
+import org.apache.jena.sparql.ARQConstants;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.core.VarExprList;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.binding.Binding;
 import org.apache.jena.sparql.engine.binding.BindingFactory;
 import org.apache.jena.sparql.engine.binding.BindingMap;
+import org.apache.jena.sparql.engine.binding.BindingProject;
 import org.apache.jena.sparql.expr.Expr;
 import org.apache.jena.sparql.expr.ExprAggregator;
+import org.apache.jena.sparql.expr.ExprLib;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.expr.aggregate.Accumulator;
+import org.apache.jena.sparql.util.Context;
+import org.apache.jena.sparql.util.NodeFactoryExtra;
+import org.apache.jena.vocabulary.RDF;
+import org.reactivestreams.Subscription;
 
-import io.reactivex.rxjava3.core.Emitter;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.FlowableEmitter;
+import io.reactivex.rxjava3.core.FlowableOnSubscribe;
+import io.reactivex.rxjava3.core.FlowableSubscriber;
+import io.reactivex.rxjava3.core.FlowableTransformer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.flowables.ConnectableFlowable;
 
 /**
  * ExecutionNode: For a given binding return a flow of subsequent bindings
@@ -40,14 +59,12 @@ interface NodeExec {
     Flowable<Binding> apply(Binding binding);
 }
 
-
 class NodeSpec {
     protected Query query;
 
 //    public Flowable<Binding> createExec(ExecutionContext ctx, Binding binding) {
 //    }
 }
-
 
 interface StreamNode {
 
@@ -75,82 +92,182 @@ interface AccPush {
     void accumulate(Binding binding);
 }
 
-class AccSubQuery
-{
+class AccSubQuery {
     protected Query query;
     protected Graph graph;
 
-
 }
 
-
-class AccFilter
-{
+class AccFilter {
     protected List<Expr> filters;
 
+}
+
+
+abstract class FlowBase<T>
+    implements FlowableSubscriber<T>
+{
+    protected FlowableEmitter<T> emitter;
+
+    public FlowBase(FlowableEmitter<T> emitter) {
+        super();
+        this.emitter = emitter;
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        emitter.onError(t);
+    }
+
+    @Override
+    public void onSubscribe(@NonNull Subscription s) {
+        emitter.setCancellable(s::cancel);
+        s.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onComplete() {
+        emitter.onComplete();
+    }
+}
+
+abstract class QueryFlowBase<T>
+    extends FlowBase<T>
+{
+    protected ExecutionContext execCxt;
+
+    public QueryFlowBase(FlowableEmitter<T> emitter, ExecutionContext execCxt) {
+        super(emitter);
+        this.execCxt = execCxt;
+    }
+}
+
+
+/**
+ * Execution of projection
+ *
+ * Based on {@link org.apache.jena.sparql.engine.iterator.QueryIterAssign}
+ * @author raven
+ *
+ */
+class AccProject
+    extends QueryFlowBase<Binding>
+{
+    protected Collection<Var> vars;
+
+    public AccProject(FlowableEmitter<Binding> emitter, ExecutionContext execCxt, Collection<Var> vars) {
+        super(emitter, execCxt);
+        this.vars = vars;
+    }
+
+    @Override
+    public void onNext(@NonNull Binding binding) {
+        Binding b = new BindingProject(vars, binding);
+        emitter.onNext(b);
+    }
+
+    public static FlowableTransformer<Binding, Binding> createTransformer(
+            ExecutionContext execCxt,
+            Collection<Var> vars) {
+        return RxUtils.createTransformer(emitter -> new AccProject(emitter, execCxt, vars));
+    }
 
 }
 
-class AccGroupBy
-//    implements Emitter<Entry<Binding, Binding>>
+/**
+ * Execution of assignment
+ *
+ * Based on {@link org.apache.jena.sparql.engine.iterator.QueryIterAssign}
+ * @author raven
+ *
+ */
+class AccAssign
+    extends QueryFlowBase<Binding>
 {
-    protected VarExprList groupVarExpr;
-    protected List<ExprAggregator> aggregators;
-    protected ExecutionContext execCxt;
+    protected VarExprList exprs;
 
+    public AccAssign(FlowableEmitter<Binding> emitter, ExecutionContext execCxt, VarExprList exprs) {
+        super(emitter, execCxt);
+        this.exprs = exprs;
+    }
 
-    protected boolean noInput = true;
-
-
-    protected Multimap<Binding, Pair<Var, Accumulator>> accumulators = MultimapBuilder.hashKeys().arrayListValues().build();
-
-    /**
-     * The delegate to emit to
-     */
-    protected Emitter<Binding> emitter;
-
-    private static Pair<Var, Accumulator> placeholder = Pair.create((Var)null, (Accumulator)null) ;
-
-    public AccGroupBy(
-        VarExprList groupVarExpr,
-        List<ExprAggregator> aggregators,
-        ExecutionContext execCxt)
-    {
-        super();
-        this.groupVarExpr = groupVarExpr;
-        this.aggregators = aggregators;
-        this.execCxt = execCxt;
+    @Override
+    public void onNext(@NonNull Binding binding) {
+        BindingMap b = BindingFactory.create(binding);
+        for (Var v : exprs.getVars()) {
+            Node n = exprs.get(v, b, execCxt);
+            if (n != null) {
+                b.add(v, n);
+            }
+        }
+        emitter.onNext(b);
     }
 
 
-    // @Override
-    public void accumulate(Binding b) {
+    public static FlowableTransformer<Binding, Binding> createTransformer(
+            ExecutionContext execCxt,
+            VarExprList exprs) {
+        return RxUtils.createTransformer(emitter -> new AccAssign(emitter, execCxt, exprs));
+    }
+
+}
+
+
+class AccGroupBy
+    extends QueryFlowBase<Binding>
+{
+    protected VarExprList groupVarExpr;
+    protected List<ExprAggregator> aggregators;
+
+    protected boolean noInput = true;
+
+    protected Multimap<Binding, Pair<Var, Accumulator>> accumulators = MultimapBuilder.hashKeys().arrayListValues()
+            .build();
+
+    private static Pair<Var, Accumulator> placeholder = Pair.create((Var) null, (Accumulator) null);
+
+    public AccGroupBy(
+            FlowableEmitter<Binding> emitter,
+            ExecutionContext execCxt,
+            VarExprList groupVarExpr,
+            List<ExprAggregator> aggregators
+            ) {
+        super(emitter, execCxt);
+        this.groupVarExpr = groupVarExpr;
+        this.aggregators = aggregators;
+    }
+
+    @Override
+    public void onNext(Binding b) {
 
         this.noInput = false;
 
-        boolean hasAggregators = ( aggregators != null && ! aggregators.isEmpty() );
-        boolean hasGroupBy = ! groupVarExpr.isEmpty();
+        boolean hasAggregators = (aggregators != null && !aggregators.isEmpty());
+        boolean hasGroupBy = !groupVarExpr.isEmpty();
         // boolean noInput = ! iter.hasNext();
 
         // Case: No input.
         // 1/ GROUP BY - no rows.
-        // 2/ No GROUP BY, e.g. COUNT=0, the results is one row always and not handled here.
+        // 2/ No GROUP BY, e.g. COUNT=0, the results is one row always and not handled
+        // here.
 
         // Case: there is input.
-        // Phase 1 : Create keys and aggreators per key, and pump bindings through the aggregators.
-        // Multimap<Binding, Pair<Var, Accumulator>> accumulators = MultimapBuilder.hashKeys().arrayListValues().build();
+        // Phase 1 : Create keys and aggreators per key, and pump bindings through the
+        // aggregators.
+        // Multimap<Binding, Pair<Var, Accumulator>> accumulators =
+        // MultimapBuilder.hashKeys().arrayListValues().build();
 
         Binding key = genKey(groupVarExpr, b, execCxt);
 
-        if ( !hasAggregators ) {
+        if (!hasAggregators) {
             // Put in a dummy to remember the input.
             accumulators.put(key, placeholder);
-            //continue;
+            // continue;
         }
 
         // Create if does not exist.
-        if ( !accumulators.containsKey(key) ) {
-            for ( ExprAggregator agg : aggregators ) {
+        if (!accumulators.containsKey(key)) {
+            for (ExprAggregator agg : aggregators) {
                 Accumulator x = agg.getAggregator().createAccumulator();
                 Var v = agg.getVar();
                 accumulators.put(key, Pair.create(v, x));
@@ -158,7 +275,7 @@ class AccGroupBy
         }
 
         // Do the per-accumulator calculation.
-        for ( Pair<Var, Accumulator> pair : accumulators.get(key) )
+        for (Pair<Var, Accumulator> pair : accumulators.get(key))
             pair.getRight().accumulate(b, execCxt);
 
         // Phase 2 : There was input and so there are some groups.
@@ -168,46 +285,49 @@ class AccGroupBy
     }
 
     public void onComplete() {
-        boolean hasAggregators = ( aggregators != null && ! aggregators.isEmpty() );
-        boolean hasGroupBy = ! groupVarExpr.isEmpty();
+        boolean hasAggregators = (aggregators != null && !aggregators.isEmpty());
+        boolean hasGroupBy = !groupVarExpr.isEmpty();
 
-        if ( noInput ) {
-            if ( hasGroupBy )
+        if (noInput) {
+            if (hasGroupBy)
                 // GROUP
-                 //return Iter.nullIterator() ;
-            if ( ! hasAggregators ) {
-                // No GROUP BY, no aggregators. One result row of no colums.
-                //return Iter.singleton(BindingFactory.binding());
-                emitter.onNext(BindingFactory.binding());
-            }
+                // return Iter.nullIterator() ;
+                if (!hasAggregators) {
+                    // No GROUP BY, no aggregators. One result row of no colums.
+                    // return Iter.singleton(BindingFactory.binding());
+                    emitter.onNext(BindingFactory.binding());
+                }
             // No GROUP BY, has aggregators. Insert default values.
             BindingMap binding = BindingFactory.create();
-            for ( ExprAggregator agg : aggregators ) {
+            for (ExprAggregator agg : aggregators) {
                 Node value = agg.getAggregator().getValueEmpty();
-                if ( value == null )
+                if (value == null)
                     continue;
                 Var v = agg.getVar();
                 binding.add(v, value);
             }
-            //return Iter.singleton(binding);
+            // return Iter.singleton(binding);
             emitter.onNext(binding);
         }
 
-        List<Binding> results = new ArrayList<>();
-        for ( Binding k : accumulators.keySet() ) {
+        for (Binding k : accumulators.keySet()) {
             Collection<Pair<Var, Accumulator>> accs = accumulators.get(k);
             BindingMap b = BindingFactory.create(k);
 
-            for ( Pair<Var, Accumulator> pair : accs ) {
+            for (Pair<Var, Accumulator> pair : accs) {
                 NodeValue value = pair.getRight().getValue();
-                if ( value == null )
+                if (value == null)
                     continue;
                 Var v = pair.getLeft();
                 b.add(v, value.asNode());
             }
-            results.add(b);
+            // results.add(b);
+            emitter.onNext(b);
         }
-        //return results.iterator();
+
+        emitter.onComplete();
+
+        // return results.iterator();
     }
 
     static private Binding genKey(VarExprList vars, Binding binding, ExecutionContext execCxt) {
@@ -218,49 +338,147 @@ class AccGroupBy
         // No group vars (implicit or explicit) => working on whole result set.
         // Still need a BindingMap to assign to later.
         BindingMap x = BindingFactory.create();
-        for ( Var var : vars.getVars() ) {
+        for (Var var : vars.getVars()) {
             Node node = vars.get(var, binding, execCxt);
             // Null returned for unbound and error.
-            if ( node != null ) {
+            if (node != null) {
                 x.add(var, node);
             }
         }
         return x;
     }
+
+
+    public static FlowableTransformer<Binding, Binding> createTransformer(
+            ExecutionContext execCxt,
+            VarExprList groupVarExpr,
+            List<ExprAggregator> aggregators) {
+        return RxUtils.createTransformer(emitter ->
+            new AccGroupBy(emitter, execCxt, groupVarExpr, aggregators));
+    }
+
+
 }
 
 
+class RxUtils {
+    /**
+     * Utils method to create a transformer from a function that takes a FlowableEmitter and
+     * yields a FlowableSubscriber from it.
+     *
+     * @param <I>
+     * @param <O>
+     * @param fsSupp
+     * @return
+     */
+    public static <I, O> FlowableTransformer<I, O> createTransformer(Function<? super FlowableEmitter<O>, ? extends FlowableSubscriber<I>> fsSupp) {
+        return upstream -> {
+            Flowable<O> result = Flowable.create(new FlowableOnSubscribe<O>() {
+                @Override
+                public void subscribe(FlowableEmitter<O> emitter) throws Exception {
+                    FlowableSubscriber<I> subscriber = fsSupp.apply(emitter);
+                    upstream.subscribe(subscriber);
+                }
+            }, BackpressureStrategy.BUFFER);
 
+            return result;
+        };
+    }
+}
 
 public class JoinStream {
-    public static Flowable<Binding> create(Query query, Supplier<? extends SparqlQueryConnection> queryConnection) {
-        return null;
+    public static Flowable<Binding> create(Query query, Supplier<? extends SparqlQueryConnection> queryConnSupp) {
+        // FIXME Close the connection; tie it to the query execution
+        // Queryexecution qe = new QueryExecution();
+        return SparqlRx.execSelectRaw(() -> queryConnSupp.get().query(query));
     }
 
+    public static ExecutionContext createExecutionContextDefault() {
+        Context context = ARQ.getContext().copy();
+        context.set(ARQConstants.sysCurrentTime, NodeFactoryExtra.nowAsDateTime());
+        ExecutionContext result = new ExecutionContext(context, null, null, null);
+        return result;
+    }
+    public static FlowableTransformer<Binding, Binding> createSink(Query query) {
+        ExecutionContext execCxt = createExecutionContextDefault();
+        FlowableTransformer<Binding, Binding> result = createTransform(query, execCxt);
+        return result;
+    }
 
-    public static AccGroupBy createSink(Query query, ExecutionContext execCxt) {
+    public static FlowableTransformer<Binding, Binding> createTransform(Query query, ExecutionContext execCxt) {
         VarExprList groupVarExpr = query.getGroupBy();
-        List<ExprAggregator> aggreators = query.getAggregators();
+        List<ExprAggregator> aggregators = query.getAggregators();
 
-        return new AccGroupBy(groupVarExpr, aggreators, execCxt);
+        // Create an updated projection
+        VarExprList rawProj = query.getProject();
 
+
+        VarExprList exprs = new VarExprList();
+        for (Var v : rawProj.getVars() )
+        {
+            Expr e = rawProj.getExpr(v) ;
+            if ( e != null )
+            {
+                Expr e2 = ExprLib.replaceAggregateByVariable(e) ;
+                exprs.add(v, e2) ;
+            } else {
+                exprs.add(v);
+            }
+            // Include in project
+            // vars.add(v) ;
+        }
+
+
+
+        Op op = Algebra.compile(query);
+        System.out.println(op);
+
+        FlowableTransformer<Binding, Binding> result = upstream -> upstream
+                .compose(AccGroupBy.createTransformer(execCxt, groupVarExpr, aggregators))
+                .compose(AccAssign.createTransformer(execCxt, exprs))
+                .compose(AccProject.createTransformer(execCxt, exprs.getVars()));
+
+        return result;
     }
+
 
     public static void main(String[] args) {
-        Sink<Binding> ac1Sink = null;
+        FlowableTransformer<Binding, Binding> accGroupBy = createSink(QueryFactory.create("SELECT (COUNT(*) + 1 AS ?c) { }"));
 
 
-        Dataset ds = null;
+
+        Dataset ds = DatasetFactory.create();
+        ds.asDatasetGraph().add(Quad.defaultGraphIRI, RDF.Nodes.type, RDF.Nodes.type, RDF.Nodes.type);
         SparqlQueryConnection conn = RDFConnectionFactory.connect(ds);
         Query query = QueryFactory.create("SELECT * { ?s ?p ?o }");
 
         Flowable<Binding> root = JoinStream.create(query, () -> conn);
 
+        Flowable<Binding> x = root.share();
+        ConnectableFlowable<Binding> publisher = x.publish();
+
+        //publisher.subscribe(y -> System.out.println("Listener 1" + y));
+        Flowable<Binding> counter = publisher.compose(accGroupBy);
+
+        counter.subscribe(count -> System.out.println("Counter saw: " + count));
+
+        publisher.subscribe(y -> System.out.println("Listener 2" + y));
+
+
+
+
+
+        // Start the whole process
+        Disposable d = publisher.connect();
+
+
+
+        // x.conn
+
+        // root.share()
 
         // Flowable.
         // root.doOnNext(onNext);
-
-
 
     }
 }
