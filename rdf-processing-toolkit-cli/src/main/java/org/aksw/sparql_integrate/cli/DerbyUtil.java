e6f757f8adbb9f3ead8cd96a6db3a86ccb6b183d
Claus Stadler <RavenArkadon@googlemail.com>
2022-04-13 14:19:21 +0200

    Minor cleanup / renamed some .sparql files to .rq

--- rdf-processing-toolkit-cli/src/main/java/org/aksw/sparql_integrate/cli/DerbyUtil.java
+++ rdf-processing-toolkit-cli/src/main/java/org/aksw/sparql_integrate/cli/DerbyUtil.java
@@ -3,6 +3,7 @@ package org.aksw.sparql_integrate.cli;
 import java.io.OutputStream;
 
 // Disable derby.log file - https://stackoverflow.com/questions/1004327/getting-rid-of-derby-log
+// System.setProperty("derby.stream.error.field", "${pkg}DerbyUtil.DEV_NULL");
 public class DerbyUtil {
     public static final OutputStream DEV_NULL = new OutputStream() {
         public void write(int b) {}
