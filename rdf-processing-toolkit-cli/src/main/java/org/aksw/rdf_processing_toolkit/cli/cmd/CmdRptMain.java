2ca3e499ceeec06280ce3b4b2eb8c375da21961f
Claus Stadler <RavenArkadon@googlemail.com>
2023-03-24 17:04:04 +0100

    Added sansa and rmltk sub-commands; not yet clean as some multiple logger issue needs yet to be resolved

--- rdf-processing-toolkit-cli/src/main/java/org/aksw/rdf_processing_toolkit/cli/cmd/CmdRptMain.java
+++ rdf-processing-toolkit-cli/src/main/java/org/aksw/rdf_processing_toolkit/cli/cmd/CmdRptMain.java
@@ -1,17 +1,21 @@
 package org.aksw.rdf_processing_toolkit.cli.cmd;
 
 import org.aksw.named_graph_stream.cli.cmd.CmdNgsMain;
+import org.aksw.rml.cli.cmd.CmdRmlTkParent;
 import org.aksw.sparql_binding_stream.cli.cmd.CmdSbsMain;
 import org.aksw.sparql_integrate.cli.cmd.CmdSparqlIntegrateMain;
 
+import net.sansa_stack.spark.cli.cmd.CmdSansaParent;
 import picocli.CommandLine.Command;
 
 @Command(name="rpt", versionProvider = VersionProviderRdfProcessingToolkit.class, description = "RDF Processing Toolkit", subcommands = {
         CmdNgsMain.class,
         CmdSparqlIntegrateMain.class,
-        CmdSbsMain.class
+        CmdSbsMain.class,
+        CmdRmlTkParent.class,
+        CmdSansaParent.class
 })
 public class CmdRptMain
-	extends CmdCommonBase
+    extends CmdCommonBase
 {
 }
