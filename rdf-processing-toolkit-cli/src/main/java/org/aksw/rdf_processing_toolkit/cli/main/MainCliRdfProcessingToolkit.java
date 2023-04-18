27564eeb3502e1d62891847231b153563dc817b5
Simon Bin <sbin@informatik.uni-leipzig.de>
2023-04-18 19:01:22 +0200

    move around static init

--- rdf-processing-toolkit-cli/src/main/java/org/aksw/rdf_processing_toolkit/cli/main/MainCliRdfProcessingToolkit.java
+++ rdf-processing-toolkit-cli/src/main/java/org/aksw/rdf_processing_toolkit/cli/main/MainCliRdfProcessingToolkit.java
@@ -1,11 +1,9 @@
 package org.aksw.rdf_processing_toolkit.cli.main;
 
-import org.aksw.rdf_processing_toolkit.cli.cmd.CliUtils;
 import org.aksw.rdf_processing_toolkit.cli.cmd.CmdRptMain;
 import org.aksw.rdf_processing_toolkit.cli.cmd.CmdUtils;
 
 public class MainCliRdfProcessingToolkit {
-    static { CliUtils.configureGlobalSettings(); }
 
     public static void main(String[] args) {
         CmdUtils.execCmd(CmdRptMain.class, args);
